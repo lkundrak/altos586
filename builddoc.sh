decb75e90cd0a417ce937dc70a4c5018e25cae28
Lubomir Rintel <lkundrak@v3.sk>
2023-11-28 14:12:59 +0100

    HDD layout

--- builddoc.sh
+++ builddoc.sh
@@ -3,4 +3,4 @@ set -x
 
 expand hdc/iop3268.asm |perl hdc/iopfmt.pl >hdc/iop3268.me
 expand hdc/hd-test.asm |perl hdc/iopfmt.pl >hdc/hd-test.me
-groff -P-pa4 -ep -t -me -Tpdf re586.me >re586.pdf
+groff -U -P-pa4 -ep -t -me -Tpdf re586.me >re586.pdf
