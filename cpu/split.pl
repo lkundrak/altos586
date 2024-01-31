f7f3601f6cc9ca68edc710ed2c63b34faecfc984
Lubomir Rintel <lkundrak@v3.sk>
2024-01-31 14:51:07 +0100

    Don't pad the firmware images to 8K

--- cpu/split.pl
+++ cpu/split.pl
@@ -3,8 +3,6 @@
 die "Usage: $0 <hi.bin> <lo.bin> <full.bin>" unless scalar @ARGV == 3;
 my ($hifile, $lofile, $srcfile) = @ARGV;
 
-my $sz = 0x2000;
-
 open my $f, $srcfile or die "$srcfile: $!";
 my @b = reverse split //, join '', <$f>;
 
@@ -16,13 +14,6 @@ while (@b) {
 	die unless scalar @b;
 	$lo .= shift @b;
 }
-
-my $len = length $hi;
-die unless $len == length $lo;
-die if $sz % $len;
-
-$hi x= $sz / $len;
-$lo x= $sz / $len;
 
 open $f, '>', $hifile or die "$hifile: $!";
 print $f $hi;
